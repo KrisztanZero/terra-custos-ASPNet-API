fe99c665233339c7072614d8ef9e579c9ce6d4dd
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-10-08 23:57:20 +0200

    create db context

---
+++ terra-custos-web-api/terra-custos-aspnet-web-api/DbContexts/TerraCustosContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using terra_custos_aspnet_web_api.Models;
+
+namespace terra_custos_aspnet_web_api.DbContexts
+{
+    public class TerraCustosContext : IdentityDbContext
+    {
+        public TerraCustosContext(DbContextOptions option) : base(option) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
