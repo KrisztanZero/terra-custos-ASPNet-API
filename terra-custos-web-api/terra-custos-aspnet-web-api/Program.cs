14b871cfa255e09d581c7ec92efafa828df1b2e7
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-10-10 19:23:57 +0200

    add services to the container

--- terra-custos-web-api/terra-custos-aspnet-web-api/Program.cs
+++ terra-custos-web-api/terra-custos-aspnet-web-api/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using terra_custos_aspnet_web_api.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TerraCustosContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("TerraCustos")));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<TerraCustosContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
