8ccb4a0ef80ea7e0ee76ff4499bdd2d13993ce8b
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-10-10 19:18:45 +0200

    create generic service

---
+++ terra-custos-web-api/terra-custos-aspnet-web-api/IService/IGenericService.cs
@@ -0,0 +1,13 @@
+﻿using terra_custos_aspnet_web_api.Models;
+
+namespace terra_custos_aspnet_web_api.IService
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(string id);
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(string id, T entity);
+        Task<bool> DeleteAsync(string id);
+    }
+}
---
+++ terra-custos-web-api/terra-custos-aspnet-web-api/Services/GenericService.cs
@@ -0,0 +1,38 @@
+﻿using terra_custos_aspnet_web_api.IRepositories;
+using terra_custos_aspnet_web_api.IService;
+
+namespace terra_custos_aspnet_web_api.Services
+{
+    public class GenericService<T> : IGenericService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+
+        public GenericService(IGenericRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+        public async Task<T> CreateAsync(T entity)
+        {
+            return await _repository.CreateAsync(entity);
+        }
+
+        public async Task<T> UpdateAsync(string id, T entity)
+        {
+            return await _repository.UpdateAsync(id, entity);
+        }
+        public async Task<bool> DeleteAsync(string id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
