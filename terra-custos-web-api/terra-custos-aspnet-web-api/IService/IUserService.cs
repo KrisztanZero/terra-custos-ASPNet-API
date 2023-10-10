5dfd7d515b884006e9dcc54fb74c78518f29a720
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-10-10 19:19:05 +0200

    create user service

---
+++ terra-custos-web-api/terra-custos-aspnet-web-api/IService/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using terra_custos_aspnet_web_api.Models;
+
+namespace terra_custos_aspnet_web_api.IService
+{
+    public interface IUserService : IGenericService<User>
+    {
+        Task<User> GetByNameAsync(string username);
+    }
+}
---
+++ terra-custos-web-api/terra-custos-aspnet-web-api/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using terra_custos_aspnet_web_api.IRepositories;
+using terra_custos_aspnet_web_api.IService;
+using terra_custos_aspnet_web_api.Models;
+
+namespace terra_custos_aspnet_web_api.Services
+{
+    public class UserService : GenericService<User> , IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IGenericRepository<User> repository, IUserRepository userRepository) : base(repository) 
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<User> GetByNameAsync(string name)
+        {
+            return  _userRepository.GetUserByNameAsync(name);
+        }
+    }
+}
